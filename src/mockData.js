a87b0207eea1ca56a3689e829ab5ff30457483c2
Raquel <rqlromo@gmail.com>
2022-10-01 15:10:55 +0100

    add images

--- src/mockData.js
+++ src/mockData.js
@@ -8,17 +8,23 @@ const mockData = {
       {
         id: uuidv4(),
         name: "Ada Lovelace",
+        image: "http://localhost:3000/assets/images/ada_lovelace.jpg",
         position: "Frontend Developer",
+        skills: ["HTML", "CSS"],
       },
       {
         id: uuidv4(),
         name: "Jon Nieve",
+        image: "http://localhost:3000/assets/images/jon_nieve.jpg",
         position: "Backend Developer",
+        skills: [".Net"],
       },
       {
         id: uuidv4(),
         name: "Arya Stark",
+        image: "http://localhost:3000/assets/images/arya_stark.jpg",
         position: "Frontend Developer",
+        skills: ["React", "JavaScript", "HTML"],
       },
     ],
   },
@@ -29,12 +35,16 @@ const mockData = {
       {
         id: uuidv4(),
         name: "Sansa Stark",
+        image: "http://localhost:3000/assets/images/sansa_stark.jpg",
         position: "Frontend Developer",
+        skills: ["JavaScript", "HTML", "CSS"],
       },
       {
         id: uuidv4(),
         name: "Daenerys Targaryen",
+        image: "http://localhost:3000/assets/images/daenerys_targaryen.jpg",
         position: "Backend Developer",
+        skills: [".Net"],
       },
     ],
   },
@@ -44,8 +54,10 @@ const mockData = {
     candidates: [
       {
         id: uuidv4(),
-        name: "Raquel Romo",
+        name: "Raquel Romo Gomez",
+        image: "http://localhost:3000/assets/images/raquel_romo_gomez.jpg",
         position: "Frontend Developer",
+        skills: ["React", "React Hooks", "JavaScript", "HTML", "CSS"],
       },
     ],
   },
