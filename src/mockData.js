dbe97ff8989e0ca295317e04aa6cd151b1c9b5e6
Raquel <rqlromo@gmail.com>
2022-10-01 11:03:13 +0100

    update mockData

--- src/mockData.js
+++ src/mockData.js
@@ -1,51 +1,54 @@
 import { v4 as uuidv4 } from "uuid";
 
-const mockData = [
-  {
-    // id: uuidv4(),
-    id: "Candidate_application_received",
-    column: "Candidate application received",
+const mockData = {
+  Application_received: {
+    id: "Application_received",
+    column: "Application received",
     candidates: [
       {
         id: uuidv4(),
         name: "Ada Lovelace",
+        position: "Frontend Developer",
       },
       {
         id: uuidv4(),
         name: "Jon Nieve",
+        position: "Backend Developer",
       },
       {
         id: uuidv4(),
         name: "Arya Stark",
+        position: "Frontend Developer",
       },
     ],
   },
-  {
-    // id: uuidv4(),
-    id: "Caroline(CEO)_Phone_Screen",
-    column: "Caroline(CEO) Phone Screen",
+  CEO_Phone_Screen: {
+    id: "CEO_Phone_Screen",
+    column: "CEO Phone Screen",
     candidates: [
       {
         id: uuidv4(),
         name: "Sansa Stark",
+        position: "Frontend Developer",
       },
       {
         id: uuidv4(),
         name: "Daenerys Targaryen",
+        position: "Backend Developer",
       },
     ],
   },
-  {
-    // id: uuidv4(),
+  Technical_Test: {
     id: "Technical_Test",
     column: "Technical Test",
     candidates: [
       {
         id: uuidv4(),
         name: "Raquel Romo",
+        position: "Frontend Developer",
       },
     ],
   },
-];
+};
 
 export default mockData;
